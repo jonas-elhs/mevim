e4acd34df1bb78c7f6de3e41c22060128ba6086a
jonas-elhs <jonas.elhs@outlook.com>
2025-11-04 15:04:03 +0100

    utils/file: moved to core/statusline/file

--- lua/ilzayn/core/ui/statusline/file.lua
+++ lua/ilzayn/core/ui/statusline/file.lua
@@ -1,10 +1,57 @@
 local utils = require("ilzayn.utils")
 
+local M = {}
+
+M.flags = {
+  modified = "",
+  locked = "󰌾",
+}
+
+M.name_overrides = {
+  ["help"] = "Help",
+  ["oil"] = "Oil",
+  ["snacks_picker_input"] = "Snacks Picker",
+}
+
+function M.get_flags(buffer)
+  buffer = buffer or 0
+
+  return vim.bo[buffer].modified
+      and M.flags.modified
+      or (vim.bo[buffer].modifiable == false or vim.bo[buffer].readonly == true)
+        and M.flags.locked
+        or ""
+end
+
+function M.get_name()
+  local name = vim.fn.fnamemodify(vim.fn.expand("%"), ":t")
+
+  local type = vim.bo.filetype
+  if M.name_overrides[type] then
+    name = M.name_overrides[type]
+  end
+
+  return name ~= "" and name or type
+end
+
+function M.get_icon()
+  local success, devicons = pcall(require, "nvim-web-devicons")
+
+  if not success then
+    return ""
+  end
+
+  return devicons.get_icon(M.get_name(), M.get_type()) or ""
+end
+
+function M.get_type()
+  return vim.bo.filetype
+end
+
 return function()
-  local icon = utils.file.get_icon()
-  local name = utils.file.get_name()
-  local flags = utils.file.get_flags()
-  -- local type = utils.file.get_type()
+  local icon = M.get_icon()
+  local name = M.get_name()
+  local flags = M.get_flags()
 
   return table.concat({
     (utils.width.more_than(35) and icon ~= "")
@@ -16,9 +63,5 @@ return function()
     (utils.width.more_than(50) and flags ~= "")
       and table.concat({ " ", flags, })
       or "",
-
-    -- (utils.width.more_than(75) and type ~= "")
-    --   and table.concat({ "  |  ", type, })
-    --   or "",
   })
 end
--- lua/ilzayn/utils/init.lua
+++ lua/ilzayn/utils/init.lua
@@ -1,7 +1,6 @@
 return {
   keymap = require("ilzayn.utils.keymap"),
   mode = require("ilzayn.utils.mode"),
-  file = require("ilzayn.utils.file"),
   color = require("ilzayn.utils.color"),
   width = require("ilzayn.utils.width"),
   highlight = require("ilzayn.utils.highlight"),
