0e4f660d935e58d4237ab18e53813c6c6c5cced9
jonas-elhs <jonas.elhs@outlook.com>
2025-11-06 16:40:31 +0100

    plugins/nvim-treesitter: renamed from treesitter

--- lua/ilzayn/plugins/code/init.lua
+++ lua/ilzayn/plugins/code/init.lua
@@ -3,6 +3,6 @@ require("ilzayn.plugins.code.lsp")
 return {
   { import = "ilzayn.plugins.code.blink" },
   { import = "ilzayn.plugins.code.gitsigns" },
-  { import = "ilzayn.plugins.code.treesitter" },
+  { import = "ilzayn.plugins.code.nvim-treesitter" },
   { import = "ilzayn.plugins.code.render-markdown" },
 }
