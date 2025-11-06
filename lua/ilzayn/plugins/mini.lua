9c057a3c8a84433eccb4ebd6421642309a5c2c5e
jonas-elhs <jonas.elhs@outlook.com>
2025-11-06 21:15:14 +0100

    plugins/mini.surround: added setup

--- lua/ilzayn/plugins/mini.lua
+++ lua/ilzayn/plugins/mini.lua
@@ -14,7 +14,18 @@ return {
     event = "BufReadPre",
 
     after = function()
-      -- require("mini.surround").setup()
+      require("mini.surround").setup({
+        mappings = {
+          add = "gsa",
+          delete = "gsd",
+          replace = "gsr",
+          find = "",
+          find_left = "",
+          highlight = "",
+        },
+
+        silent = true,
+      })
     end,
   },
   {
