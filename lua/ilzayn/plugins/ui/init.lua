4ec4430130934d90532cde767fcc8d201d43e800
jonas-elhs <jonas.elhs@outlook.com>
2025-11-06 16:39:17 +0100

    plugins/nvim-colorizer: renamed from colorizer

--- lua/ilzayn/plugins/ui/init.lua
+++ lua/ilzayn/plugins/ui/init.lua
@@ -2,7 +2,7 @@ return {
   { import = "ilzayn.plugins.ui.noice" },
   { import = "ilzayn.plugins.ui.incline" },
   { import = "ilzayn.plugins.ui.helpview" },
-  { import = "ilzayn.plugins.ui.colorizer" },
+  { import = "ilzayn.plugins.ui.nvim-colorizer" },
   { import = "ilzayn.plugins.ui.colorful-winsep" },
   { import = "ilzayn.plugins.ui.nvim-web-devicons" },
 }
