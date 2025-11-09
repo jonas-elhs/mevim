3d523cbdcf5eca3fe9a67aa78cdc6f0f281a222c
jonas-elhs <jonas.elhs@outlook.com>
2025-11-09 19:51:54 +0100

    plugins/snacks: fix bufdelete mapping

--- lua/ilzayn/plugins/snacks.lua
+++ lua/ilzayn/plugins/snacks.lua
@@ -110,7 +110,7 @@ return {
 
       -- BufDelete
       map("n", "<leader>bx", function() Snacks.bufdelete.delete() end, "Exit open buffer")
-      map("n", "<leader>bx!", function() Snacks.bufdelete.delete({ force = true }) end, "Force exit open buffer")
+      map("n", "<leader>bX", function() Snacks.bufdelete.delete({ force = true }) end, "Force exit open buffer")
 
       -- LazyGit
       map("n", "<leader>g", function() Snacks.lazygit.open() end, "Open LazyGit")
