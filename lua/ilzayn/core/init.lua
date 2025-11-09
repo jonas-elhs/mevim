f14d74761f464ff3521439d6e400a407110bc6b4
jonas-elhs <jonas.elhs@outlook.com>
2025-11-09 14:48:28 +0100

    core/theme: load in init

--- lua/ilzayn/core/init.lua
+++ lua/ilzayn/core/init.lua
@@ -1,4 +1,5 @@
+require("ilzayn.core.ui")
+require("ilzayn.core.theme")
 require("ilzayn.core.options")
 require("ilzayn.core.mappings")
 require("ilzayn.core.highlights")
-require("ilzayn.core.ui")
