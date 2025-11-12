12ec7bc868587872f1bdd59b4337e54a9741f916
jonas-elhs <jonas.elhs@outlook.com>
2025-11-12 18:42:10 +0100

    plugins/blink-cmp: moved from plugins/blink

--- lua/ilzayn/plugins/code/init.lua
+++ lua/ilzayn/plugins/code/init.lua
@@ -1,9 +1,9 @@
 require("ilzayn.plugins.code.lsp")
 
 return {
-  { import = "ilzayn.plugins.code.blink" },
   { import = "ilzayn.plugins.code.treesj" },
   { import = "ilzayn.plugins.code.gitsigns" },
+  { import = "ilzayn.plugins.code.blink-cmp" },
   { import = "ilzayn.plugins.code.nvim-treesitter" },
   { import = "ilzayn.plugins.code.render-markdown" },
   { import = "ilzayn.plugins.code.markdown-preview" },
