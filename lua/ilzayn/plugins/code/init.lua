d9d552b9413bb2f0d75b5d6d8d51136973b3703d
jonas-elhs <jonas.elhs@outlook.com>
2025-11-16 14:28:58 +0100

    plugins/gitsigns: deleted

--- lua/ilzayn/plugins/code/init.lua
+++ lua/ilzayn/plugins/code/init.lua
@@ -4,7 +4,6 @@ return {
   { import = "ilzayn.plugins.code.lazydev" },
   { import = "ilzayn.plugins.code.trouble" },
   { import = "ilzayn.plugins.code.conform" },
-  { import = "ilzayn.plugins.code.gitsigns" },
   { import = "ilzayn.plugins.code.nvim-treesitter" },
   { import = "ilzayn.plugins.code.render-markdown" },
   { import = "ilzayn.plugins.code.markdown-preview" },
