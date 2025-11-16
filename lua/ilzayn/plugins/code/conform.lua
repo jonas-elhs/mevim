e4e2a20e1988d62c5b4bc3f65f05d106394d4de9
jonas-elhs <jonas.elhs@outlook.com>
2025-11-16 10:28:28 +0100

    plugins/conform: add autoformat enable/disable/toggle commands and keymaps

--- lua/ilzayn/plugins/code/conform.lua
+++ lua/ilzayn/plugins/code/conform.lua
@@ -2,10 +2,11 @@ return {
   {
     "conform.nvim",
 
-    cmd = { "ConformInfo" },
+    cmd = { "ConformInfo", "FormatToggle", "FormatEnable", "FormatDisable" },
     -- stylua: ignore
     keys = {
       { "<leader>f", function() require("conform").format() end, desc = "Format buffer" },
+      { "<leader>F", "<CMD>FormatToggle<CR>", desc = "Toggle autoformat" },
     },
 
     after = function()
@@ -15,10 +16,24 @@ return {
           nix = { "alejandra" },
           rust = { "rustfmt" },
         },
-        format_on_save = {
-          timeout_ms = 500,
-        },
+        format_on_save = function(buffer)
+          if vim.g.disable_autoformat or vim.b[buffer].disable_autoformat then
+            return
+          end
+
+          return { timeout_ms = 500 }
+        end,
       })
+
+      vim.api.nvim_create_user_command("FormatToggle", function()
+        vim.g.disable_autoformat = not vim.g.disable_autoformat
+      end, { desc = "Toggle autoformat" })
+      vim.api.nvim_create_user_command("FormatEnable", function()
+        vim.g.disable_autoformat = false
+      end, { desc = "Enable autoformat" })
+      vim.api.nvim_create_user_command("FormatDisable", function()
+        vim.g.disable_autoformat = true
+      end, { desc = "Disable autoformat" })
     end,
   },
 }
