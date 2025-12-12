fac70afddf7445e32bb24292f10ffa0040daa408
jonas-elhs <jonas.elhs@outlook.com>
2025-12-12 20:07:59 +0100

    core/autocommands: highlight yank region

--- lua/jonas/core/autocommands.lua
+++ lua/jonas/core/autocommands.lua
@@ -2,7 +2,7 @@ local group = vim.api.nvim_create_augroup
 local autocmd = vim.api.nvim_create_autocmd
 
 -- Show Cursorline Only In Current Window
-local cursorline_group = group("JonasCursorline", {})
+local cursorline_group = group("jonas/cursorline", {})
 autocmd({ "VimEnter", "WinEnter", "BufWinEnter", "TermLeave" }, {
   group = cursorline_group,
   callback = function()
@@ -13,5 +13,14 @@ autocmd({ "WinLeave" }, {
   group = cursorline_group,
   callback = function()
     vim.wo.cursorline = false
+  end,
+})
+
+-- Highlight Yank Region
+local yank_highlight_group = group("jonas/yank_highlight", {})
+autocmd({ "TextYankPost" }, {
+  group = yank_highlight_group,
+  callback = function()
+    vim.hl.on_yank()
   end,
 })
