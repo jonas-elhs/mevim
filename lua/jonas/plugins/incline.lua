35ed88b16986ce30d20e06cd31db9102693e7132
jonas-elhs <jonas.elhs@outlook.com>
2026-01-05 13:48:09 +0100

    plugins/incline: use 'MiniIcons' instead of requiring the module, display read only status

--- lua/jonas/plugins/incline.lua
+++ lua/jonas/plugins/incline.lua
@@ -17,26 +17,27 @@ require("incline").setup({
   },
 
   render = function(props)
-    local icons = require("mini.icons")
     local focused = props.focused
 
     local content_highlight = focused and "JonasCurrentMode" or "JonasInactive"
     local separator_highlight = content_highlight .. "Reverse"
 
     local modified = vim.bo[props.buf].modified
+    local read_only = not vim.bo[props.buf].modifiable or vim.bo[props.buf].readonly
     local file_name = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
-    local icon = icons.get("file", file_name)
+    local icon = MiniIcons.get("file", file_name)
 
     return {
       { "", group = separator_highlight },
 
       {
-        icon and { " ", icon } or "",
+        icon and " " .. icon or "",
 
         " ",
 
         file_name ~= "" and file_name or "[No Name]",
         modified and " " or "",
+        read_only and " 󰌾" or "",
 
         " ",
 
