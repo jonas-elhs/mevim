8b0fa5ea2fee049e732eac355f52df3172b5ac39
jonas-elhs <jonas.elhs@outlook.com>
2026-01-06 09:21:19 +0100

    plugins/mini: sort

--- lua/jonas/plugins/mini.lua
+++ lua/jonas/plugins/mini.lua
@@ -68,10 +68,10 @@ require("mini.surround").setup({
 -- mini.hipatterns
 require("mini.hipatterns").setup({
   highlighters = {
-    fixme = { pattern = "%f[%w]()FIXME()%f[%W]", group = "MiniHipatternsFixme" },
     hack = { pattern = "%f[%w]()HACK()%f[%W]", group = "MiniHipatternsHack" },
     todo = { pattern = "%f[%w]()TODO()%f[%W]", group = "MiniHipatternsTodo" },
     note = { pattern = "%f[%w]()NOTE()%f[%W]", group = "MiniHipatternsNote" },
+    fixme = { pattern = "%f[%w]()FIXME()%f[%W]", group = "MiniHipatternsFixme" },
   },
 })
 
