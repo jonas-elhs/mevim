a57ecfd915e596d586fb56455abb8a8f350c45f5
jonas-elhs <jonas.elhs@outlook.com>
2026-01-11 15:12:46 +0100

    plugins/treesj: remove unnecessary and slow setup call

--- lua/jonas/plugins/treesj.lua
+++ lua/jonas/plugins/treesj.lua
@@ -1,8 +1,3 @@
-require("treesj").setup({
-  max_join_length = 9999,
-  use_default_keymaps = false,
-})
-
 vim.keymap.set("n", "gs", function()
   require("treesj").toggle()
 end, { desc = "Toggle node under cursor" })
