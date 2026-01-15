ea15973a8f2ac064d8dfad8515e4ce2b8685a4cb
Jonas Elhs <jonas.elhs@outlook.com>
2026-01-15 17:18:03 +0100

    core/autocommands: move tree-sitter setup from plugins/nvim-treesitter

--- lua/jonas/core/autocommands.lua
+++ lua/jonas/core/autocommands.lua
@@ -1,6 +1,22 @@
 local group = vim.api.nvim_create_augroup
 local autocmd = vim.api.nvim_create_autocmd
 
+-- Enable Tree-Sitter
+autocmd("FileType", {
+  callback = function()
+    local started = pcall(vim.treesitter.start)
+
+    if not started then
+      return
+    end
+
+    vim.o.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+    vim.o.foldmethod = "expr"
+
+    vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+  end,
+})
+
 -- Show Cursorline Only In Current Window
 local cursorline_group = group("jonas/cursorline", {})
 autocmd({ "VimEnter", "WinEnter", "BufWinEnter", "TermLeave" }, {
