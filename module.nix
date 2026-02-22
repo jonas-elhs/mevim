8fd25b9f110d6b4f6dcc7a36ad10fe5151083ff0
Jonas Elhs <jonas.elhs@outlook.com>
2026-02-22 13:17:52 +0100

    fix(core/lsp): pass nixpkgs to nixd

--- module.nix
+++ module.nix
@@ -128,6 +128,9 @@ in {
     ];
 
     info = {
+      nixd = {
+        nixpkgs = "import ${builtins.path {inherit (pkgs) path;}} {}";
+      };
       codelldb = {
         executablePath = "${pkgs.vscode-extensions.vadimcn.vscode-lldb}/share/vscode/extensions/vadimcn.vscode-lldb/adapter/codelldb";
       };
