b00166f964fff860c1a5d56af5566039c38f9c2b
Jonas Elhs <jonas.elhs@outlook.com>
2026-03-31 13:14:14 +0200

    feat(plugins/leap): add remote action keymaps

--- lua/jonas/plugins/leap.lua
+++ lua/jonas/plugins/leap.lua
@@ -2,4 +2,21 @@ require("leap").setup({
   safe_labels = "",
 })
 
-vim.keymap.set({ "n", "x", "o" }, "s", "<Plug>(leap-anywhere)")
+vim.keymap.set({ "n", "x" }, "s", "<Plug>(leap-anywhere)")
+
+-- Remote action
+vim.keymap.set({ "n", "x", "o" }, "S", function()
+  require("leap.remote").action()
+end)
+
+-- Remote text object
+for _, ai in ipairs({ "a", "i" }) do
+  vim.keymap.set({ "x", "o" }, ai .. "r", function()
+    local ok, char = pcall(vim.fn.getcharstr)
+    if not ok or (char == vim.keycode("<ESC>")) then
+      return
+    end
+
+    require("leap.remote").action({ input = ai .. char })
+  end)
+end
