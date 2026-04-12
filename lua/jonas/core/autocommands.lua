c06c3fd13c7759b10c6523c169041cce89619c77
Jonas Elhs <jonas.elhs@outlook.com>
2026-04-12 11:47:34 +0200

    feat(autocommands): create parent dir if it doesn't exist

--- lua/jonas/core/autocommands.lua
+++ lua/jonas/core/autocommands.lua
@@ -144,3 +144,17 @@ autocmd("FileType", {
     })
   end,
 })
+
+-- Create parent dir if it doesn't exist
+local parent_dir = group("jonas/create_parent_dir", {})
+autocmd("BufWritePre", {
+  group = parent_dir,
+  callback = function(event)
+    if event.match:match("^%w%w+://") then
+      return
+    end
+
+    local file = vim.uv.fs_realpath(event.match) or event.match
+    vim.fn.mkdir(vim.fn.fnamemodify(file, ":p:h"), "p")
+  end,
+})
